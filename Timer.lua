18058e3c0205c747674183067ce7b7b143d88fe2
Hannan <Abdulhanan45k@gmail.com>
2025-10-04 22:58:22 +0300

    added timer.lua module

---
+++ Timer.lua
@@ -0,0 +1,18 @@
+local object = {}
+object.__index = object
+
+
+function object.new(Name)
+    local self = setmetatable({}, object)
+    self.Name = Name
+    self.Speed = 0
+    --any other characteristics an object can have
+    --eg self.Transparency
+
+
+    --type shit
+
+    return self
+end
+
+--
